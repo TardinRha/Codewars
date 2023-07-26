6e2c338704ad1fe08aad8e0d41007df72aae5a0f
Rhaissa Tardin <rha.tardin@gmail.com>
2023-07-26 13:50:23 -0300

    Enumerable Magic #25

---
+++ Kata's resolution/EnumerableMagic#25/Program.cs
@@ -0,0 +1,38 @@
+﻿//Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+namespace EnumerableMagic_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter numbers separated by spaces: ");
+            int[] inputArray = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            Console.Write("Enter the number of elements to take: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] result = Kata.Take(inputArray, n);
+
+            Console.Write("First n elements from the list: ");
+            foreach (var element in result)
+            {
+                Console.Write($"{element} ");
+            }
+        }
+    }
+    public static class Kata
+    {
+        public static int[] Take(int[] arr, int n)
+        {
+            if (arr == null || arr.Length == 0) { return new int[] { }; }
+            if (n > arr.Length) { n = arr.Length; }
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = arr[i];
+            }
+            return a;
+        }
+    }
+}
