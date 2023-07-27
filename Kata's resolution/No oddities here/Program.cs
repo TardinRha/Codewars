480aff99a9efdbb9b794bb3a4e586a992b8fec0a
Rhaissa Tardin <rha.tardin@gmail.com>
2023-07-27 17:13:10 -0300

    No_oddities_here

---
+++ Kata's resolution/No oddities here/Program.cs
@@ -0,0 +1,37 @@
+﻿//Write a small function that returns the values of an array that are not odd.
+//All values in the array will be integers. Return the good values in the order they are given.
+
+namespace No_oddities_here
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numbers separated by spaces: ");
+            int[] inputArray = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            int[] result = NoOddities.NoOdds(inputArray);
+
+            Console.WriteLine("Values that are not odd:");
+            foreach (var value in result)
+            {
+                Console.Write($"{value} ");
+            }
+        }
+    }
+    public class NoOddities
+    {
+        public static int[] NoOdds(int[] values)
+        {
+            List<int> arr = new();
+            foreach (int v in values)
+            {
+                if (v % 2 == 0)
+                {
+                    arr.Add(v);
+                }
+            }
+            return arr.ToArray();
+        }
+    }
+}
