8db786c1e0e4a7f2fa97378fd74ac86242b1102a
Rhaissa Tardin <rha.tardin@gmail.com>
2023-08-01 14:49:55 -0300

    .

---
+++ Kata's resolution/Convert a string to an array/Program.cs
@@ -0,0 +1,20 @@
+﻿//Write a function to split a string and convert it into an array of words.
+
+namespace Convert_a_string_to_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    public class Solution
+    {
+        public static string[] StringToArray(string str)
+        {
+            string[] palavras = str.Split(" ");
+            return palavras;
+        }
+    }
+}
