abc2bb56342ddbebb0dd28ef5f03442320e50fba
Rhaissa Tardin <rha.tardin@gmail.com>
2023-08-02 10:03:21 -0300

    Summing_a_number_s_digits

---
+++ Kata's resolution/Summing a number's digits/Program.cs
@@ -0,0 +1,27 @@
+﻿//Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits. For example: (Input-- > Output)
+
+namespace Summing_a_number_s_digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Sum of digits: {Kata.SumDigits(number)}");
+        }
+    }
+    public class Kata
+    {
+        public static int SumDigits(int number)
+        {
+            string numero = number.ToString().Replace("-", "");
+            int soma = 0;
+            foreach (char n in numero)
+            {
+                soma += int.Parse(n.ToString());
+            }
+            return soma;
+        }
+    }
+}
