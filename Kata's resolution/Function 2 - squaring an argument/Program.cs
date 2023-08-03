c3e6dbf2c287c6597b3b4e190426ddf3e5b3fa1b
Rhaissa Tardin <rha.tardin@gmail.com>
2023-08-03 10:05:42 -0300

    Function_2___squaring_an_argument

---
+++ Kata's resolution/Function 2 - squaring an argument/Program.cs
@@ -0,0 +1,21 @@
+﻿//Now you have to write a function that takes an argument and returns the square of it.
+
+namespace Function_2___squaring_an_argument
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double number = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Square of the number: {Kata.Square(number)}");
+        }
+    }
+    public class Kata
+    {
+        public static double Square(double n)
+        {
+            return n * n;
+        }
+    }
+}
